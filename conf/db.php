d240ec812ae3071c7254e1e5ddd19116dcf384da
Al7Mino <36192701+Al7Mino@users.noreply.github.com>
2019-01-29 16:38:31 +0100

    Add files via upload

---
+++ conf/db.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Voici les détails de connexion à la base de données
+ */  
+define("HOST", "localhost");     // L’hébergeur où vous voulez vous connecter.
+define("USER", "root");    // Le nom d’utilisateur de la base de données.
+define("PASSWORD", "");    // Le mot de passe de la base de données. 
+define("DATABASE", "ludisep");    // Le nom de la base de données.
+?>
---
+++ conf/web.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Voici les définitions des variables globales du site
+ */  
+define('FIRST_DATA_URL', 2);
+define('SITE_ROOT', realpath(dirname(__DIR__)));
+
+define('BASE_URI', 'http://localhost/ludisep');
+
+define('CSS_DIR', BASE_URI.'/css');
+define('SCRIPT_DIR', BASE_URI.'/script');
+define('IMG_DIR', BASE_URI.'/img');
+
+?>
